777f6aa522308dd222fe82a1e42bb15b1a2d080f
Alison <sidneialison@gmail.com>
2018-06-06 13:26:18 -0300

    Questão 6 alterada

--- Questao_6.cpp
+++ Questao_6.cpp
@@ -8,33 +8,12 @@ class Tempo
 private :
 	int hh, mm, ss;
 public :
-	// Le os dados do tempo a partir da entrada padrao
-	void lerTempo(void);
-	// Retorna o tempo em segundos
+
 	int converteEmSegundos(void);
-	// Imprime o tempo no formato HH:MM:SS e o seu total
-	// em segundos
-	void mostraTempo(void);
 
 	friend istream& operator>>(istream& in, Tempo& T);
 	friend ostream& operator<<(ostream& out, Tempo& T);
 };
-
-// Implementar os metodos ...
-
-void
-Tempo::lerTempo(){
-	struct tm time;
-
-	cout << "Digite o tempo no formato \"HH:MM:SS\" : ";
-
-	cin >> get_time(&time,"%T");;
-
-	hh = time.tm_hour;
-	mm = time.tm_min;
-	ss = time.tm_sec;
-
-}
 
 int
 Tempo::converteEmSegundos(){
@@ -43,18 +22,21 @@ Tempo::converteEmSegundos(){
 	return sec;
 }
 
-void
-Tempo::mostraTempo(){
-	cout << hh << ":" << mm << ":" << ss << " tem o total de " << converteEmSegundos() << " segundos." << endl;
-}
-
 istream& operator>>(istream& in, Tempo& T){
-	T.lerTempo();
+	struct tm time;
+
+	cout << "Digite o tempo no formato \"HH:MM:SS\" : ";
+
+	cin >> get_time(&time,"%T");;
+
+	T.hh = time.tm_hour;
+	T.mm = time.tm_min;
+	T.ss = time.tm_sec;
 	return in;
 }
 
 ostream& operator<<(ostream& out, Tempo& T){
-	T.mostraTempo();
+	cout << T.hh << ":" << T.mm << ":" << T.ss << " tem o total de " << T.converteEmSegundos() << " segundos." << endl;
 	return out;
 }
 
